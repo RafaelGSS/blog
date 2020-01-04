601dd43a3ec6391ec63ca6d36df24ae4599d65cc
RafaelGSS <rafael.nunu@hotmail.com>
2020-01-04 19:25:00 -0300

    feat: add phoenix app with mysql

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :blog,
+  ecto_repos: [Blog.Repo]
+
+# Configures the endpoint
+config :blog, BlogWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "O9t7ET62OGTExZL+47OQnTrCWu6h+3f1QqQg53Q4Ep05/jzkQcAh2adFoqPTzrE1",
+  render_errors: [view: BlogWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Blog.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
