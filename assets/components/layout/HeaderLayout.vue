03410d5ec0c419bf85ee0c6ff268b2480057eab6
RafaelGSS <rafael.nunu@hotmail.com>
2020-01-08 19:38:05 -0300

    feat(component): add Header component

--- assets/components/layout/HeaderLayout.vue
+++ assets/components/layout/HeaderLayout.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    Header from component
-  </div>
+  <header>
+    <h1>
+      <a href="https://blog.sebastian-daschner.com/"><strong>sebastiandaschner</strong> blog</a>
+    </h1>
+    <hr>
+    <ul>
+      <li>
+        <a href="https://www.sebastian-daschner.com/about">About</a>
+      </li>
+      <li>
+        <a href="https://www.sebastian-daschner.com/publications">Publications</a>
+      </li>
+      <li>
+        <a href="https://www.sebastian-daschner.com/offerings/">Offerings</a>
+      </li>
+      <li>
+        <a href="https://www.sebastian-daschner.com/contact">Contact</a>
+      </li>
+      <li>
+        <a href="https://blog.sebastian-daschner.com/">Blog</a>
+      </li>
+      <li>
+        <a href="https://www.sebastian-daschner.com/news/">News</a>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
