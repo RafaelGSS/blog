db20452e932ae6d3b5d9bb5594405aedefb740bc
RafaelGSS <rafael.nunu@hotmail.com>
2020-01-10 00:16:38 -0300

    feat(components): add item component

--- assets/components/layout/FooterLayout.vue
+++ assets/components/layout/FooterLayout.vue
@@ -11,5 +11,24 @@ export default {
 </script>
 
 <style>
+footer {
+  max-width: 45em;
+  margin: 0 auto;
+  padding: 2.5em 0 2.5em 0;
+  height: 1.8em;
+}
 
+footer span {
+  float: right;
+}
+
+@media (max-width: 46.875em) {
+  footer {
+    padding: 1em 0;
+  }
+
+  footer span {
+    padding-right: 1em;
+  }
+}
 </style>
