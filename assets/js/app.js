db54e47a982f3b2130eff07a42ba2693f0736f8d
RafaelGSS <rafael.nunu@hotmail.com>
2020-01-11 01:31:33 -0300

    feat(SSR): removed components vuejs and use SSR native phoenix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,1 @@
-import Vue from 'vue'
-
 import css from '../css/app.css'
-
-import Home from '../components/pages/Home'
-
-window.Vue = Vue
-const app = new Vue({
-  el: '#app',
-  components: {
-    Home
-  }
-})
--- lib/blog_web/controllers/page_controller.ex
+++ lib/blog_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule BlogWeb.PageController do
   use BlogWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render conn, "index.html", posts: [
+      %{
+        title: "Example of greatest title",
+        subtitle: "thursday, january 09, 2020",
+        text: "I’ve created a few videos on the topic of effective enterprise testing. I still see a huge importance in this topic in real-world projects. Here are my experiences in testing Enterprise Java projects together with some examples.",
+        link: "#"
+      }
+    ]
   end
 end
