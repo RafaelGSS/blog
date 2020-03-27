b32ca04519911888039f75794860f53e1a76a740
RafaelGSS <rafael.nunu@hotmail.com>
2020-03-27 00:30:54 -0300

    chore(prod): add logger level debug

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :blog, BlogWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
