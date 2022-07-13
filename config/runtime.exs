f10c76e7753e01286962e282ba9fbf5950937de7
RafaelGSS <rafael.nunu@hotmail.com>
2022-07-13 00:09:46 -0300

    chore: bind local network

--- config/runtime.exs
+++ config/runtime.exs
@@ -37,15 +37,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :blog, BlogWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    http: [ip: {0, 0, 0, 0}, port: 4000],
     secret_key_base: secret_key_base
 
   # ## Configuring the mailer
