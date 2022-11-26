665753cf0b2c7df5f9eb4957c165cacdc3b9585a
RafaelGSS <rafael.nunu@hotmail.com>
2022-11-26 18:34:55 -0300

    style: improve blog ux

--- lib/blog/services/markdown_service.ex
+++ lib/blog/services/markdown_service.ex
@@ -1,5 +1,5 @@
 defmodule Blog.Services.MarkdownService do
   def parse_as_html(markdown) do
-    Earmark.as_html!(markdown)
+    Earmark.as_html!(markdown, %Earmark.Options{code_class_prefix: "language- lang-"})
   end
 end
